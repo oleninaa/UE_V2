48fe59bb82b50084e091f78c4f43c7dbc6d2d620
oleninaa <96290422+oleninaa@users.noreply.github.com>
2021-12-18 10:11:24 +0300

    Update Level_Migrate_UE4_31-HTML5-Shipping.UE4.js

--- Level_Migrate_UE4_31-HTML5-Shipping.UE4.js
+++ Level_Migrate_UE4_31-HTML5-Shipping.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Level_Migrate_UE4_31-HTML5-Shipping.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/oleninaa/UE_V2/8a3ffb6122957fd00ae3d386500479f958685552/Level_Migrate_UE4_31-HTML5-Shipping.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
