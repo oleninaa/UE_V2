ef8ad7c35598eafa9ff88f5fce212ff451b35547
oleninaa <96290422+oleninaa@users.noreply.github.com>
2021-12-18 10:15:43 +0300

    Update Level_Migrate_UE4_31-HTML5-Shipping.UE4.js

--- Level_Migrate_UE4_31-HTML5-Shipping.UE4.js
+++ Level_Migrate_UE4_31-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/oleninaa/UE_V2/48fe59bb82b50084e091f78c4f43c7dbc6d2d620/UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
