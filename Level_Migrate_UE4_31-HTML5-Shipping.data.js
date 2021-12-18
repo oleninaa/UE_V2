8a3ffb6122957fd00ae3d386500479f958685552
oleninaa <96290422+oleninaa@users.noreply.github.com>
2021-12-18 10:08:05 +0300

    Update Level_Migrate_UE4_31-HTML5-Shipping.data.js

--- Level_Migrate_UE4_31-HTML5-Shipping.data.js
+++ Level_Migrate_UE4_31-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/albert/Documents/Migrate/V3/Level_Migrate_UE4_31/Binaries/HTML5/Level_Migrate_UE4_31-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'Level_Migrate_UE4_31-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/cnn1aqtkr062dyt/Level_Migrate_UE4_31-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/cnn1aqtkr062dyt/Level_Migrate_UE4_31-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
